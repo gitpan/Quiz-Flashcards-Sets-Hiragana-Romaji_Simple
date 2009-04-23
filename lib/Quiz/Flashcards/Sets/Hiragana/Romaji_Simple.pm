7ee63348fb3754302f4ea2121cd330e9a84950d1
Christian Walde <walde.christian@gmail.com>
2009-04-23 17:59:11 +0000

    Import of MITHALDU/Quiz-Flashcards-Sets-Hiragana-Romaji_Simple-0.04 from CPAN.
    gitpan-cpan-distribution: Quiz-Flashcards-Sets-Hiragana-Romaji_Simple
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MITHALDU/Quiz-Flashcards-Sets-Hiragana-Romaji_Simple-0.04.tar.gz
    gitpan-cpan-author:       MITHALDU
    gitpan-cpan-maturity:     released

--- lib/Quiz/Flashcards/Sets/Hiragana/Romaji_Simple.pm
+++ lib/Quiz/Flashcards/Sets/Hiragana/Romaji_Simple.pm
@@ -14,11 +14,11 @@ Quiz::Flashcards::Sets::Hiragana::Romaji_Simple - Flashcard set with the basic 4
 
 =head1 VERSION
 
-Version 0.01
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 DESCRIPTION
 
@@ -69,52 +69,52 @@ under the same terms as Perl itself.
 my $ab = "Quiz::Flashcards::Audiobanks::Japanese_Syllables";
 
 my @set;
-$set[0] = { question => "あ", answer => "a", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "a.wav"};
-$set[1] = { question => "か", answer => "ka", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ka.wav"};
-$set[2] = { question => "さ", answer => "sa", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "sa.wav"};
-$set[3] = { question => "た", answer => "ta", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ta.wav"};
-$set[4] = { question => "な", answer => "na", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "na.wav"};
-$set[5] = { question => "は", answer => "ha", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ha.wav"};
-$set[6] = { question => "ま", answer => "ma", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ma.wav"};
-$set[7] = { question => "や", answer => "ya", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ya.wav"};
-$set[8] = { question => "ら", answer => "ra", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ra.wav"};
-$set[9] = { question => "わ", answer => "wa", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "wa.wav"};
-$set[10] = { question => "ん", answer => "n", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "n.wav"};
-$set[11] = { question => "い", answer => "i", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "i.wav"};
-$set[12] = { question => "き", answer => "ki", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ki.wav"};
-$set[13] = { question => "し", answer => "shi", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "shi.wav"};
-$set[14] = { question => "ち", answer => "chi", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "chi.wav"};
-$set[15] = { question => "に", answer => "ni", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ni.wav"};
-$set[16] = { question => "ひ", answer => "hi", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "hi.wav"};
-$set[17] = { question => "み", answer => "mi", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "mi.wav"};
-$set[18] = { question => "ゆ", answer => "yu", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "yu.wav"};
-$set[19] = { question => "り", answer => "ri", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ri.wav"};
-$set[20] = { question => "う", answer => "u", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "u.wav"};
-$set[21] = { question => "く", answer => "ku", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ku.wav"};
-$set[22] = { question => "す", answer => "su", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "su.wav"};
-$set[23] = { question => "つ", answer => "tsu", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "tsu.wav"};
-$set[24] = { question => "ぬ", answer => "nu", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "nu.wav"};
-$set[25] = { question => "ふ", answer => "fu", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "hu.wav"};
-$set[26] = { question => "む", answer => "mu", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "mu.wav"};
-$set[27] = { question => "る", answer => "ru", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ru.wav"};
-$set[28] = { question => "え", answer => "e", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "e.wav"};
-$set[29] = { question => "け", answer => "ke", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ke.wav"};
-$set[30] = { question => "せ", answer => "se", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "se.wav"};
-$set[31] = { question => "て", answer => "te", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "te.wav"};
-$set[32] = { question => "ね", answer => "ne", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ne.wav"};
-$set[33] = { question => "へ", answer => "he", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "he.wav"};
-$set[34] = { question => "め", answer => "me", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "me.wav"};
-$set[35] = { question => "よ", answer => "yo", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "yo.wav"};
-$set[36] = { question => "れ", answer => "re", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "re.wav"};
-$set[37] = { question => "お", answer => "o", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "o.wav"};
-$set[38] = { question => "こ", answer => "ko", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ko.wav"};
-$set[39] = { question => "そ", answer => "so", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "so.wav"};
-$set[40] = { question => "と", answer => "to", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "to.wav"};
-$set[41] = { question => "の", answer => "no", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "no.wav"};
-$set[42] = { question => "ほ", answer => "ho", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ho.wav"};
-$set[43] = { question => "も", answer => "mo", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "mo.wav"};
-$set[44] = { question => "ろ", answer => "ro", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ro.wav"};
-$set[45] = { question => "を", answer => "wo", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "o.wav"};
+push @set, { question => "あ", complexity => 1, answer => "a", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "a.wav" };
+push @set, { question => "い", complexity => 1, answer => "i", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "i.wav" };
+push @set, { question => "う", complexity => 1, answer => "u", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "u.wav" };
+push @set, { question => "え", complexity => 1, answer => "e", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "e.wav" };
+push @set, { question => "お", complexity => 1, answer => "o", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "o.wav" };
+push @set, { question => "か", complexity => 2, answer => "ka", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ka.wav" };
+push @set, { question => "き", complexity => 2, answer => "ki", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ki.wav" };
+push @set, { question => "く", complexity => 2, answer => "ku", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ku.wav" };
+push @set, { question => "け", complexity => 2, answer => "ke", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ke.wav" };
+push @set, { question => "こ", complexity => 2, answer => "ko", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ko.wav" };
+push @set, { question => "さ", complexity => 3, answer => "sa", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "sa.wav" };
+push @set, { question => "し", complexity => 3, answer => "shi", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "shi.wav" };
+push @set, { question => "す", complexity => 3, answer => "su", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "su.wav" };
+push @set, { question => "せ", complexity => 3, answer => "se", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "se.wav" };
+push @set, { question => "そ", complexity => 3, answer => "so", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "so.wav" };
+push @set, { question => "た", complexity => 4, answer => "ta", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ta.wav" };
+push @set, { question => "ち", complexity => 4, answer => "chi", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "chi.wav" };
+push @set, { question => "つ", complexity => 4, answer => "tsu", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "tsu.wav" };
+push @set, { question => "て", complexity => 4, answer => "te", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "te.wav" };
+push @set, { question => "と", complexity => 4, answer => "to", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "to.wav" };
+push @set, { question => "な", complexity => 5, answer => "na", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "na.wav" };
+push @set, { question => "に", complexity => 5, answer => "ni", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ni.wav" };
+push @set, { question => "ぬ", complexity => 5, answer => "nu", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "nu.wav" };
+push @set, { question => "ね", complexity => 5, answer => "ne", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ne.wav" };
+push @set, { question => "の", complexity => 5, answer => "no", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "no.wav" };
+push @set, { question => "は", complexity => 6, answer => "ha", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ha.wav" };
+push @set, { question => "ひ", complexity => 6, answer => "hi", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "hi.wav" };
+push @set, { question => "ふ", complexity => 6, answer => "fu", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "hu.wav" };
+push @set, { question => "へ", complexity => 6, answer => "he", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "he.wav" };
+push @set, { question => "ほ", complexity => 6, answer => "ho", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ho.wav" };
+push @set, { question => "ま", complexity => 7, answer => "ma", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ma.wav" };
+push @set, { question => "み", complexity => 7, answer => "mi", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "mi.wav" };
+push @set, { question => "む", complexity => 7, answer => "mu", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "mu.wav" };
+push @set, { question => "め", complexity => 7, answer => "me", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "me.wav" };
+push @set, { question => "も", complexity => 7, answer => "mo", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "mo.wav" };
+push @set, { question => "ら", complexity => 8, answer => "ra", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ra.wav" };
+push @set, { question => "り", complexity => 8, answer => "ri", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ri.wav" };
+push @set, { question => "る", complexity => 8, answer => "ru", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ru.wav" };
+push @set, { question => "れ", complexity => 8, answer => "re", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "re.wav" };
+push @set, { question => "ろ", complexity => 8, answer => "ro", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ro.wav" };
+push @set, { question => "わ", complexity => 9, answer => "wa", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "wa.wav" };
+push @set, { question => "を", complexity => 9, answer => "wo", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "o.wav" };
+push @set, { question => "や", complexity => 9, answer => "ya", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "ya.wav" };
+push @set, { question => "ゆ", complexity => 9, answer => "yu", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "yu.wav" };
+push @set, { question => "よ", complexity => 9, answer => "yo", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "yo.wav" };
+push @set, { question => "ん", complexity => 9, answer => "n", question_type => "text", answer_type => "text", audiobank => $ab, audio_file => "n.wav" };
 
 
 
